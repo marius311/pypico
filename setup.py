65975962c68ec640d3b294f880720d482d562df1
marius <mmillea@ucdavis.edu>
2012-05-31 04:42:53 -0700

    update to v3, better README

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from numpy.distutils.core import setup, Extension
 
 setup(
-    name='PICO',
-    version='0.1.0',
+    name='pypico',
+    version='3.0.0',
     author='Marius Millea',
     author_email='mmillea@ucdavis.edu',
     packages=['pypico'],
-    url='http://pypi.python.org/pypi/PICO/',
+    url='http://pypi.python.org/pypi/pypico/',
     license='LICENSE.txt',
     description='Quickly compute the CMB powerspectra and matter transfer functions.',
     long_description=open('README').read(),
