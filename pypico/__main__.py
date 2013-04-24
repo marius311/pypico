89c5eda6e3465e3edd8af9c386ffc97a83478a7b
marius <mmillea@ucdavis.edu>
2013-04-23 17:43:57 -0700

    removed existing pico

--- pypico/__main__.py
+++ pypico/__main__.py
@@ -6,9 +6,8 @@ parser.add_argument('code_file',nargs=1,help='path to Python module which contai
 parser.add_argument('data_file',nargs=1,help='output data file')
 
 parser.add_argument('--args',nargs='*',metavar='<args>',help='args to pass to get_pico(*args)')
-parser.add_argument('--existing_pico',nargs=1,metavar='<data_file>',help='redo an existing data file')
 
 if not sys.argv[1:]: parser.print_help()
 else:
     args = vars(parser.parse_args())
-    pypico.create_pico(args['code_file'][0],args['data_file'][0],args=args['args'] or [], existing_pico=args['existing_pico'][0])
+    pypico.create_pico(args['code_file'][0],args['data_file'][0],args=args['args'] or [])
